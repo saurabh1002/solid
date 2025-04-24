55dae2f32e91803b3d7d0193a0e0c2876f95d69a
ssg1002 <sgupta2@uni-bonn.de>
2025-04-24 12:38:36 +0200

    make a bit efficient, remove redundant ops

--- solid/core/solid.py
+++ solid/core/solid.py
@@ -15,49 +15,47 @@ class SOLiDModule:
         self.gap_height = ((self.fov_u - self.fov_d)) / self.num_elevation   
 
     def xy2theta(self, x, y):
-        if (x >= 0 and y >= 0): 
-            theta = 180 / np.pi * np.arctan(y/x)
-        if (x < 0 and y >= 0): 
-            theta = 180 - ((180 / np.pi) * np.arctan(y / (-x)))
-        if (x < 0 and y < 0): 
-            theta = 180 + ((180 / np.pi) * np.arctan(y / x))
-        if ( x >= 0 and y < 0):
-            theta = 360 - ((180 / np.pi) * np.arctan((-y) / x))
+        theta = np.empty_like(x, dtype=float)
+
+        mask1 = (x >= 0) & (y >= 0)
+        mask2 = (x < 0) & (y >= 0)
+        mask3 = (x < 0) & (y < 0)
+        mask4 = (x >= 0) & (y < 0)
+
+        theta[mask1] = 180 / np.pi * np.arctan(y[mask1] / x[mask1])
+        theta[mask2] = 180 - (180 / np.pi) * np.arctan(y[mask2] / (-x[mask2]))
+        theta[mask3] = 180 + (180 / np.pi) * np.arctan(y[mask3] / x[mask3])
+        theta[mask4] = 360 - (180 / np.pi) * np.arctan((-y[mask4]) / x[mask4])
+
         return theta
 
-    def pt2rah(self, point):
-        x = point[0]
-        y = point[1]
-        z = point[2]
+    def pt2rah(self, points):
+        x = points[:, 0]
+        y = points[:, 1]
+        z = points[:, 2]
         
-        if(x == 0.0):
-            x = 0.001  
-        if(y == 0.0):
-            y = 0.001 
+        x = np.where(x == 0.0, 0.001, x)
+        y = np.where(y == 0.0, 0.001, y)
 
         theta   = self.xy2theta(x, y) 
-        faraway = np.sqrt(x * x + y * y) 
+        faraway = np.sqrt(np.square(x) + np.square(y))
         phi     = np.rad2deg(np.arctan2(z, faraway)) - self.fov_d
 
         idx_ring   = np.divmod(faraway, self.gap_ring)[0]      
         idx_sector = np.divmod(theta, self.gap_sector)[0]   
         idx_height = np.divmod(phi, self.gap_height)[0]
         
-        if(idx_ring >= self.num_range):
-            idx_ring = self.num_range - 1
+        idx_ring = np.where(idx_ring >= self.num_range, self.num_range - 1, idx_ring)
+        idx_height = np.where(idx_height >= self.num_elevation, self.num_elevation - 1, idx_height)
 
-        if(idx_height >= self.num_elevation):
-            idx_height = self.num_elevation - 1
-
-        return int(idx_ring), int(idx_sector), int(idx_height)
+        return idx_ring.astype(int), idx_sector.astype(int), idx_height.astype(int)
 
     def get_descriptor(self, scan):
         rh_counter = np.zeros([self.num_range, self.num_elevation])             
         sh_counter = np.zeros([self.num_angle, self.num_elevation])   
-        for point in scan: 
-            idx_ring, idx_sector, idx_height = self.pt2rah(point) 
-            rh_counter[idx_ring, idx_height] = rh_counter[idx_ring, idx_height] + 1     
-            sh_counter[idx_sector, idx_height] = sh_counter[idx_sector, idx_height] + 1  
+        idx_rings, idx_sectors, idx_heights = self.pt2rah(scan)
+        rh_counter[idx_rings, idx_heights] = rh_counter[idx_rings, idx_heights] + 1
+        sh_counter[idx_sectors, idx_heights] = sh_counter[idx_sectors, idx_heights] + 1
      
         ring_matrix = rh_counter    
         sector_matrix = sh_counter
